3910dfac74a08dbf0c25de7da1bee9521026d92d
Alemnew Marie <alemnewmarie461@gmail.com>
2021-12-04 15:10:35 +0300

    Use vuex to manage state, user imgur api for authentication

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <h4>Image Upload App</h4>
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
+// const CLIENT_ID = '5dbfaa9b405b7f8'
+// const CLIENT_SECRET = '4f07be1d9ddc3bb9335bb4a450460f55cf7be865'
 
 export default {
   name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a href="/" class="active item"> ImageUpload </a>
+    <div class="right menu">
+      <a class="ui item" @click="login"> Login </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: "Navbar",
+  methods: {
+    ...mapActions(['login']),
+  }
+};
+</script>
+
+<style scoped>
+/* nav {
+  display: flex;
+  align-content: center;
+  justify-content: space-between;
+} */
+</style>
